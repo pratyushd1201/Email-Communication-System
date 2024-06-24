faec9bdaab17c7a4e7108a52b4a15b0e612b0da5
Pratyush Das <pratyush.d1201@gmail.com>
2024-06-25 00:45:09 +0530

    Delete Backend directory

--- Backend/postmark.js
+++
@@ -1,6 +0,0 @@
-const postmark = require("postmark");
-
-const serverToken = process.env.NEXT_PUBLIC_POSTMARK_SERVER_TOKEN;
-const client = new postmark.ServerClient(serverToken);
-
-export {client}
